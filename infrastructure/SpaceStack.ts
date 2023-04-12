cf01aaa76a42ad89e881d4050de813e5501812ed
Michael Duberstein <mduberstein@gmail.com>
2023-04-11 22:10:24 -0400

    In Clip 27 AWS API Gateway and Lambda, after adding the ApiGateway to SpaceStack.ts

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -2,9 +2,12 @@ import {Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda'
 import { join } from 'path';
-
+import { LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway'
 
 export class SpaceStack extends Stack {
+    private api = new RestApi(this, 'SpaceApi');
+    private helloLambda: LambdaFunction;
+
     constructor(scope: Construct, id: string,  props: StackProps) {
         super(scope, id, props)
         const helloLambda = new LambdaFunction (this, 'helloLambda', {
@@ -12,5 +15,13 @@ export class SpaceStack extends Stack {
             code: Code.fromAsset(join( __dirname, '..', 'services', 'hello')),
             handler: 'hello.main'
         });
+
+        // Hello Api lambda integration:
+        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+        const helloLambdaResource = this.api.root.addResource('hello');
+        helloLambdaResource.addMethod('GET', helloLambdaIntegration);
     }
+
+
+
 } 
