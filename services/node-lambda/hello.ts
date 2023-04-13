fc0c4cf849cefdaa87af866c9491e95e682b0711
Michael Duberstein <mduberstein@gmail.com>
2023-04-13 07:13:03 -0400

    In Clip 36  Using the AWS sdk, before fixing the Access problem

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,9 +1,14 @@
-import { v4 } from 'uuid'
+import { v4 } from 'uuid';
+import { S3 } from 'aws-sdk';
+
+const s3Client = new S3();
+
 async function handler(event: unknown, context: any) {
+   const buckets = await s3Client.listBuckets().promise();
    console.log('in handler of node lambda');
    return {
       statusCode: 200,
-      body: 'Hello from a ts Lambda' +v4()
+      body: 'Here are your buckets: ' + JSON.stringify(buckets.Buckets)
    }
 }
 
