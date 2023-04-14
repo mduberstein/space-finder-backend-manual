000d15373966ca8c3cc1d127f57928ea0f2cab30
Michael Duberstein <mduberstein@gmail.com>
2023-04-13 20:06:48 -0400

    After Clip 39 Put Item

---
+++ services/SpacesTable/Create.ts
@@ -0,0 +1,30 @@
+import { AWSError, DynamoDB } from 'aws-sdk';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { v4 } from 'uuid';
+
+const dbClient = new DynamoDB.DocumentClient();
+
+async function handler (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+   const result: APIGatewayProxyResult = {
+      statusCode: 200,
+      body: 'Hello from DynamoDb'
+   };
+
+   const item = {
+      spaceId : v4()
+   }
+   try {
+      await dbClient.put({
+         TableName: 'SpacesTable',
+         Item: item
+      }).promise(); 
+   } catch(error: unknown) {
+      const typedError = error as AWSError;
+      result.body = typedError.message;
+   }
+   return result;
+
+}
+
+
+export {handler};
