df6c7bcc03d4a80d3c210cc6fb4a3dffbefe6ad3
Michael Duberstein <mduberstein@gmail.com>
2023-04-14 09:57:58 -0400

    In Clip 44 Query Operation, before synth and deploy Read lambda

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,5 +1,7 @@
 // import { handler } from '../../services/SpacesTable/Create';
+import { APIGateway } from 'aws-sdk';
 import { handler as readHandler } from '../../services/SpacesTable/Read'
+import { APIGatewayProxyEvent } from 'aws-lambda';
 
 // // Create
 // const event = {
@@ -9,9 +11,26 @@ import { handler as readHandler } from '../../services/SpacesTable/Read'
 // }
 // handler (event as any, {} as any);
 
-// Read
-const result = readHandler({} as any, {} as any);
+// Read, Clip 43, scan
+// const result = readHandler({} as any, {} as any);
+// result.then((apiResult)=>{
+//    const items = JSON.parse(apiResult.body);
+//    console.log(items);
+// })
+
+// Read, Clip 44, scan
+const event: APIGatewayProxyEvent = {
+   queryStringParameters: {
+      spaceId: 'f87d915e-57bf-458f-8fec-33cd2de58391'
+   }
+} as any;
+const result = readHandler(event, {} as any);
 result.then((apiResult)=>{
    const items = JSON.parse(apiResult.body);
    console.log(items);
 })
+
+
+
+
+
