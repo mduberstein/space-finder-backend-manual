b4bdec674cb61f3ebf5c57605a44ffa228186886
Michael Duberstein <mduberstein@gmail.com>
2023-04-13 20:42:36 -0400

    After Clip 41 DynamoDB lambda initial setup, GenericLambda class updated and instantiated

--- infrastructure/GenericTable.ts
+++ infrastructure/GenericTable.ts
@@ -1,12 +1,33 @@
 // type Table in {} and intellisense will suggest the lib
 import { Stack } from "aws-cdk-lib";
 import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import { join } from 'path';
 
+export interface TableProps {
+   tableName: string,
+   primaryKey: string
+   createLambdaPath?: string,
+   readLambdaPath?: string,
+   updateLambdaPath?: string,
+   deleteLambdaPath?: string,
+
+}
 export class GenericTable {
    private table: Table;
 
-	constructor(private name: string, private primaryKey: string,
-			private stack: Stack) {
+   private createLambda: NodejsFunction | undefined;
+   private readLambda: NodejsFunction | undefined;
+   private updateLambda: NodejsFunction | undefined;
+   private deleteLambda: NodejsFunction | undefined;
+
+   public createLambdaIntegration: LambdaIntegration;
+   public readLambdaIntegration: LambdaIntegration;
+   public updateLambdaIntegration: LambdaIntegration;
+   public deleteLambdaIntegration: LambdaIntegration;
+
+	constructor(private stack: Stack, private props: TableProps) {
 			this.initialize();
 	}
 
@@ -15,12 +36,20 @@ export class GenericTable {
 	}
 
 	private createTable() {
-		this.table = new Table(this.stack, this.name, {
+		this.table = new Table(this.stack, this.props.tableName, {
 			partitionKey: {
-				name: this.primaryKey,
+				name: this.props.primaryKey,
 				type: AttributeType.STRING
 			},
-         tableName: this.name
+         tableName: this.props.tableName
 		});
+   }
+
+   private createSingleLambda(lambdaName: string): NodejsFunction {
+      const lambdaId = `${this.props.tableName}-${lambdaName}`
+      return new NodejsFunction(this.stack, lambdaId, {
+         entry: join(__dirname, '..', 'services', this.props.tableName, `${lambdaName}.ts`),
+         handler: 'handler'
+      })   
    }
 }
--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -9,11 +9,12 @@ import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class SpaceStack extends Stack {
    private api = new RestApi(this, 'SpaceApi');
-   private spacesTable = new GenericTable(
-      'SpacesTable',
-      'spaceId',
-      this
-   )
+
+   private spacesTable = new GenericTable(this, {
+      tableName: 'SpacesTable',
+      primaryKey: 'spaceId',
+      createLambdaPath: 'Create'
+   })
 
    constructor(scope: Construct, id: string,  props: StackProps) {
       super(scope, id, props)
