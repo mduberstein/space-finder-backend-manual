146eb511e4f99ec72cc19a931079e285e7e6cc3c
Michael Duberstein <mduberstein@gmail.com>
2023-04-14 08:33:13 -0400

    After Clip 43 Scan Operation

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,8 +1,17 @@
-import { handler } from '../../services/SpacesTable/Create';
+// import { handler } from '../../services/SpacesTable/Create';
+import { handler as readHandler } from '../../services/SpacesTable/Read'
 
-const event = {
-   body: {
-      location: 'Paris'
-   }
-}
-handler (event as any, {} as any);
+// // Create
+// const event = {
+//    body: {
+//       location: 'Paris'
+//    }
+// }
+// handler (event as any, {} as any);
+
+// Read
+const result = readHandler({} as any, {} as any);
+result.then((apiResult)=>{
+   const items = JSON.parse(apiResult.body);
+   console.log(items);
+})
