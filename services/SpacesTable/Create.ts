57b98f46322f45c2a7ed4b24a88ea3fd42f2ac6c
Michael Duberstein <mduberstein@gmail.com>
2023-04-15 17:05:31 -0400

    In Clip 48 Data Validation, before adding exception type checking in the catch statement

--- services/SpacesTable/Create.ts
+++ services/SpacesTable/Create.ts
@@ -1,5 +1,6 @@
 import { AWSError, DynamoDB } from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { MissingFieldError, validateAsSpaceEntry } from '../Shared/InputValidator';
 import { v4 } from 'uuid';
 
 const TABLE_NAME = process.env.TABLE_NAME;
@@ -11,19 +12,21 @@ async function handler (event: APIGatewayProxyEvent, context: Context): Promise<
       body: 'Hello from DynamoDb'
    };
 
-   const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
-   item.spaceId = v4();
-
    try {
+      const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
+      item.spaceId = v4();
+      validateAsSpaceEntry(item);
       await dbClient.put({
          TableName: TABLE_NAME!,
          Item: item
       }).promise(); 
+      result.body = JSON.stringify(`Created item with id: ${item.spaceId}`);
    } catch(error: unknown) {
+      result.statusCode = 403;
       const typedError = error as AWSError;
       result.body = typedError.message;
    }
-   result.body = JSON.stringify(`Created item with id: ${item.spaceId}`);
+ 
    return result;
 
 }
--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,13 +1,12 @@
-// Clip 39
-// import { handler } from '../../services/SpacesTable/Create';
-import { APIGateway } from 'aws-sdk';
-// Clip 43, 44, 45
+// Clip 39, 48
+import { handler } from '../../services/SpacesTable/Create';
+// //Clip 43, 44, 45
 // import { handler as readHandler } from '../../services/SpacesTable/Read'
 import { APIGatewayProxyEvent } from 'aws-lambda';
-// Clip 46
+// //Clip 46
 // import { handler as updateHandler } from '../../services/SpacesTable/Update'
-// Clip 47
-import { handler as deleteHandler } from '../../services/SpacesTable/Delete'
+// //Clip 47
+// import { handler as deleteHandler } from '../../services/SpacesTable/Delete'
 // // Create
 // const event = {
 //    body: {
@@ -16,14 +15,14 @@ import { handler as deleteHandler } from '../../services/SpacesTable/Delete'
 // }
 // handler (event as any, {} as any);
 
-// Read, Clip 43, scan
+// //Read, Clip 43, scan
 // const result = readHandler({} as any, {} as any);
 // result.then((apiResult)=>{
 //    const items = JSON.parse(apiResult.body);
 //    console.log(items);
 // })
 
-// Read, Clip 44, scan
+// //Read, Clip 44, scan
 // const event: APIGatewayProxyEvent = {
 //    queryStringParameters: {
 //       spaceId: 'f87d915e-57bf-458f-8fec-33cd2de58391'
@@ -58,18 +57,32 @@ import { handler as deleteHandler } from '../../services/SpacesTable/Delete'
 //    console.log(items);
 // })
 
-// Delete, Clip 47
+// // Delete, Clip 47
+// const event: APIGatewayProxyEvent = {
+//    queryStringParameters: {
+//       spaceId: '227f4990-b3f0-4d6c-ba2e-580c5cd2260a'
+//    },
+// } as any;
+
+// const result = deleteHandler(event, {} as any);
+// result.then((apiResult)=>{
+//    const items = JSON.parse(apiResult.body);
+//    console.log(items);
+// })
+
+// Validate and Create, Clip 48
 const event: APIGatewayProxyEvent = {
-   queryStringParameters: {
-      spaceId: '227f4990-b3f0-4d6c-ba2e-580c5cd2260a'
+   body: {
+      name: 'some name'
    },
 } as any;
 
-const result = deleteHandler(event, {} as any);
+const result = handler(event, {} as any);
 result.then((apiResult)=>{
    const items = JSON.parse(apiResult.body);
    console.log(items);
 })
+
 
 
 
