ba567ab4dc2c1320b29c1611eccfaa863aa282c1
Michael Duberstein <mduberstein@gmail.com>
2023-04-16 14:48:26 -0400

    After Clip 53 Generating AWS Cognito tokens

---
+++ test/playground/Auth/AuthService.ts
@@ -0,0 +1,21 @@
+import { Auth, Amplify } from 'aws-amplify';
+import { config } from './config'
+import { CognitoUser } from '@aws-amplify/auth';
+
+
+// there was no intellisense or type information here
+Amplify.configure ({
+   Auth: {
+      mandatorySignIn: false,
+      region: config.REGION,
+      userPoolId: config.USER_POOL_ID,
+      userPoolWebClientId: config.APP_CLIENT_ID,
+      authenticationFlowType: 'USER_PASSWORD_AUTH'
+   }
+})
+export class AuthService {
+   public async login(userName: string, password: string){
+      const user = await Auth.signIn(userName, password) as CognitoUser;
+      return user;
+   }
+}
---
+++ test/playground/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+   REGION: 'us-east-2',
+   USER_POOL_ID: 'us-east-2_yDPp6jQvn',
+   APP_CLIENT_ID: '71u1hnfitmmvue6eplj39mugoo',
+   TEST_USER_NAME: 'atoz',
+   TEST_USER_PASSWORD: 'Dev4321!'
+}
